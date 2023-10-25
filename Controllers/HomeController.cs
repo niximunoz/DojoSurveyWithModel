4a3799735c846acae597b51d5bdcac84295db0cb
niximunoz <niximunozr@gmail.com>
2023-10-25 19:45:00 -0300

    Dojo Survey with Model

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("survey")]
+    public IActionResult Submission(Usuario usuario)
+    {
+
+        return View("result", usuario);
+    }
+
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/HomeModel.cs
@@ -0,0 +1,9 @@
+#pragma warning disable CS8618
+namespace DojoSurveyWithModel.Models;
+public class Usuario
+{
+    public string FirstName {get;set;}
+    public string Location {get;set;}
+    public string Language {get;set;}
+    public string Comments {get;set;}
+}
